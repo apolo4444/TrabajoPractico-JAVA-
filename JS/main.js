2be803a789f08b474945eeeed09151c5daaaa27c
Apolo4444 <shizoudoragon@gmail.com>
2023-10-28 18:20:10 -0300

    Nueva Actualizacion

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,89 @@
+
+const defaultTicketPrice = 200
+
+const fields = [
+   {
+      "fieldName": "nombre",
+      "regexPattern": /^[a-z ,.'-]+$/i
+   },
+   {
+      "fieldName": "apellido",
+      "regexPattern": /^[a-z ,.'-]+$/i
+   },
+   {
+      "fieldName": "mail",
+      "regexPattern": /\S+@\S+\.\S+/
+   },
+   {
+      "fieldName": "cantidad",
+      "regexPattern": /^[0-9]+$/
+   }
+]
+
+const categories = [
+   {
+      "index": 1,
+      "name": "Estudiante",
+      "desc": 0.8
+   },
+   {
+      "index": 2,
+      "name": "Estudiante",
+      "desc": 0.5
+   },
+   {
+      "index": 3,
+      "name": "Estudiante",
+      "desc": 0.15
+   }
+]
+
+function calcularPrecioTicket() {
+   var allFieldsValid = fields.every(isFieldValid)
+   if (allFieldsValid){
+      var categoria = document.getElementById("categoria").value;
+      if (categoria <= 0){
+         alert("No se selecciono categoria.")
+      } else {
+         var cant = document.getElementById("cantidad").value
+         var desc = categories.find(f => f.index == categoria).desc;
+         document.getElementById("totalPagar").innerText = (defaultTicketPrice * cant)*(1 - desc);
+      }
+   } 
+   // else {
+   //    alert("Hay errores (f)!")
+   // }
+}
+
+
+function isFieldValid(fieldProperties) {
+   var field = document.getElementById(fieldProperties.fieldName);
+   field.classList.remove("is-invalid")
+   focusIfEmpty(field, fieldProperties);
+   validateWithRegex(field, fieldProperties);
+   return !field.classList.contains("is-invalid")
+}
+
+function focusIfEmpty(field, fieldProperties) {
+   if (field.value === ""){
+      field.classList.add("is-invalid");
+      alert("Falta ingresar el campo: " + fieldProperties.fieldName)
+      field.focus();
+   }
+}
+
+function validateWithRegex(field, fieldProperties){
+   alert(field.value)
+   alert(fieldProperties.regexPattern)
+   if (!fieldProperties.regexPattern.test(field.value)){
+      alert("Error de validacion en el campo: " + fieldProperties.fieldName)
+      field.classList.add("is-invalid");
+   }
+}
+
+document.getElementById("btnResumen").addEventListener("click", calcularPrecioTicket);
+
+/*
 function calcularTotal(){
     var name=document.getElementById("nombre");
     var surname=document.getElementById("apellido");
@@ -40,7 +126,7 @@ function calcularTotal(){
        alert("Falta ingresar una cantidad");
        categoria.focus();
     }
-    */
+    
    
     if(visualViewport.value==0){
        resumenTotal=200*cant.value;
@@ -64,3 +150,4 @@ function calcularTotal(){
    
    const BTNCLICK= document.getElementById("btnResumen");
    BTNCLICK.addEventListener("click",calcularTotal);
+   */
